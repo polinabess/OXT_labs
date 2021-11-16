0bda5d29c8b1b56368cf34fa1b0636ff80370c0e
Shosetsu01 <romamarin16@gmail.com>
2021-11-16 21:04:54 +0300

    Допилил навигацию через JS, переписал несколько строчек CSS  сразу в HTML и удалил css-файл

--- js/so2.js
+++ js/so2.js
@@ -1,72 +1,34 @@
+// Функция переключения между картинками на главной странице:
+// Сначала создается массив значений value и после происходит навигация на событие onchange для checkBox
 var checkBoxProgramm = document.getElementsByName('programm');
-
 for ( var i = 0; i < checkBoxProgramm.length; i++) {
-    checkBoxProgramm[i].onchange = changeImage;
+    checkBoxProgramm[i].addEventListener('change', function() {
+        document.querySelector('#so2_img_new').src = './img/so2/' + this.value + '.svg';
+    });   
 }
 
-function changeImage() {
-    document.querySelector('#so2_img_new').src = './img/so2/' + this.value + '.svg';
-}
-
+// Определение текущего положения селектора 
 function getSelectedCheckboxValues(name) {
     const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
     let values = [];
     checkboxes.forEach((checkbox) => {
-        values.push(checkbox.id);
+        values.push(checkbox.value);
     });
     return values;
 }
 
-const btn = document.querySelector('#btn_start');
-btn.addEventListener('click', (event) => {
-    if (getSelectedCheckboxValues('programm') == 'equilibrium') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#firstPage').classList.add('active');
-    }
-    else if (getSelectedCheckboxValues('programm') == 'reaction-spead') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#secondPage').classList.add('active');
-    }
-    else if (getSelectedCheckboxValues('programm') == 'optimal-temp') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#thirdPage').classList.add('active');
-    }
-    else if (getSelectedCheckboxValues('programm') == 'contact-time') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#fourthPage').classList.add('active');
-    }
-    else if (getSelectedCheckboxValues('programm') == 'degree-of-transformation') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#fifthPage').classList.add('active');
-    }
-    else if (getSelectedCheckboxValues('programm') == 'reactor-promtepl') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#sixthPage').classList.add('active');
-    }
-    else if (getSelectedCheckboxValues('programm') == 'reactor-vvodholgaz') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#seventhPage').classList.add('active');
-    }
-    else if (getSelectedCheckboxValues('programm') == 'reactor-promvvodholvoz') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#eighthPage').classList.add('active');
-    }
-    else if (getSelectedCheckboxValues('programm') == 'reactor-dkda') {
-        document.querySelector('#startPage').classList.remove('active');
-        document.querySelector('#ninthPage').classList.add('active');
-    }
+// Обработчик события нажатия на кнопку "Начнем.."
+const btnStart = document.querySelector('#btn_start');
+btnStart.addEventListener('click', (event) => {
+    let pageName = '#Page_'+getSelectedCheckboxValues('programm');
+    document.querySelector('#startPage').classList.remove('active');
+    document.querySelector(pageName).classList.add('active');
 });
 
-
-document.querySelector('#btn_back').addEventListener('click', (event) => {
-    document.querySelector('#firstPage').classList.remove('active');
-    document.querySelector('#secondPage').classList.remove('active');
-    document.querySelector('#thirdPage').classList.remove('active');
-    document.querySelector('#fourthPage').classList.remove('active');
-    document.querySelector('#fifthPage').classList.remove('active');
-    document.querySelector('#sixthPage').classList.remove('active');
-    document.querySelector('#seventhPage').classList.remove('active');
-    document.querySelector('#eighthPage').classList.remove('active');
-    document.querySelector('#ninthPage').classList.remove('active');
+// Обработчик события нажатия на кнопку "В начало"
+const btnBack = document.querySelector('#btn_back');
+btnBack.addEventListener('click', (event) => {
+    let pageName = '#Page_'+getSelectedCheckboxValues('programm');
     document.querySelector('#startPage').classList.add('active');
+    document.querySelector(pageName).classList.remove('active');
 });
